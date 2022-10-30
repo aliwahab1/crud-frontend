78541631df3ee3eec78c51d608c4dbde405ffb3d
aliwahab <youssufsany@gmail.com>
2022-10-30 04:50:00 +0100

    frontend design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import Todoform from "./Components/Todoform";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppStyled>
+      <div className="content-container">
+        <Todoform />
+      </div>
+    </AppStyled>
   );
 }
+
+const AppStyled = styled.div`
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .content-container {
+    background-color: #374954;
+    width: 80%;
+    height: 80vh;
+    border-top-left-radius: 20px;
+    border-bottom-left-radius: 20px;
+    box-shadow: 10px 12px 20px rgba(0, 0, 0, 0.2);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    form {
+      input,
+      textarea {
+        width: 100%;
+        padding: 0.7rem 1rem;
+        font-size: inherit;
+        font-family: inherit;
+        outline: none;
+        border: none;
+        border-radius: 15px;
+        filter: drop-shadow(0px 4px 24px rgba(0, 0, 0, 0.25));
+        margin: 0.5rem 0;
+        background-color: #4f6877;
+        color: white;
+        resize: none;
+      }
+    }
+  }
+`;
 
 export default App;
